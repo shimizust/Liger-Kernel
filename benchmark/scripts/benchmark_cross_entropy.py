4b295b8d5b09991ce7fc3420d2faae39d528396c
Steven Shimizu <shimizust@gmail.com>
2024-09-02 19:24:34 +0000

    Working benchmarking script

--- benchmark/scripts/benchmark_cross_entropy.py
+++ benchmark/scripts/benchmark_cross_entropy.py
@@ -3,254 +3,22 @@ import os
 import torch
 import triton
 from torch.nn import CrossEntropyLoss
-from utils import _test_memory, get_current_file_directory, get_gpu_name, update_benchmark_data_csv, SingleBenchmarkRunInput, BenchmarkData
+from utils import _test_memory, get_current_file_directory, get_gpu_name, update_benchmark_data_csv, SingleBenchmarkRunInput, SingleBenchmarkRunOuput, BenchmarkData, get_mean_std_stats, LIGER_KERNEL_VERSION, run_benchmarks
 from typing import List, Union, Dict, Any, Callable, Optional
 from liger_kernel.transformers.cross_entropy import LigerCrossEntropyLoss
 import json
 from dataclasses import dataclass
 import time
 
-# @triton.testing.perf_report(
-#     [
-#         triton.testing.Benchmark(
-#             x_names=["V"],
-#             x_vals=[2**i for i in range(12, 18)],
-#             xlabel="vocab size",
-#             line_arg="provider",
-#             line_vals=["liger", "huggingface"],
-#             line_names=[
-#                 "Liger",
-#                 "Hugging Face",
-#             ],
-#             styles=[
-#                 ("blue", "solid"),
-#                 ("orange", "solid"),
-#             ],
-#             ylabel="time (ms)",
-#             plot_name="cross-entropy-fwd-speed-benchmark",
-#             args={"B": 8, "T": 2048, "mode": "forward", "dtype": torch.bfloat16},
-#         ),
-#         triton.testing.Benchmark(
-#             x_names=["V"],
-#             x_vals=[2**i for i in range(12, 18)],
-#             xlabel="vocab size",
-#             line_arg="provider",
-#             line_vals=["liger", "huggingface"],
-#             line_names=["Liger", "Hugging Face"],
-#             styles=[
-#                 ("blue", "solid"),
-#                 ("orange", "solid"),
-#             ],
-#             ylabel="time (ms)",
-#             plot_name="cross-entropy-full-speed-benchmark",
-#             args={"B": 8, "T": 2048, "mode": "full", "dtype": torch.bfloat16},
-#         ),
-#     ]
-# )
-# def bench_speed_cross_entropy(B, T, V, provider, mode, dtype, device="cuda"):
-#     torch_ce = CrossEntropyLoss()
-#     liger_ce = LigerCrossEntropyLoss()
 
-#     _input = torch.randn(B * T, V, requires_grad=True, device="cuda")
-#     target = torch.randint(V, (B * T, 1), device="cuda").squeeze(1)
-
-#     def fwd():
-#         if provider == "liger":
-#             return liger_ce(_input, target)
-#         else:
-#             return torch_ce(_input, target)
-
-#     quantiles = [0.5, 0.2, 0.8]
-
-#     if mode == "forward":
-#         ms, min_ms, max_ms = triton.testing.do_bench(fwd, quantiles=quantiles, rep=100)
-#     elif mode == "backward":
-#         y = fwd()
-
-#         ms, min_ms, max_ms = triton.testing.do_bench(
-#             lambda: y.backward(retain_graph=True),
-#             quantiles=quantiles,
-#             grad_to_none=[_input],
-#             rep=100,
-#         )
-#     elif mode == "full":
-
-#         def full():
-#             y = fwd()
-#             y.backward()
-
-#         ms, min_ms, max_ms = triton.testing.do_bench(full, quantiles=quantiles, rep=100)
-
-    
-#     return ms, min_ms, max_ms
-
-
-
-
-def bench_speed_cross_entropy(input: SingleBenchmarkRunInput):
+def bench_memory_cross_entropy(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOuput:
     torch_ce = CrossEntropyLoss()
     liger_ce = LigerCrossEntropyLoss()
 
     V = input.x
     provider = input.kernel_provider
-    mode = input.kernel_operation_mode
-    print(mode)
     B = input.extra_benchmark_config["B"]
     T = input.extra_benchmark_config["T"]
-
-    _input = torch.randn(B * T, V, requires_grad=True, device="cuda")
-    target = torch.randint(V, (B * T, 1), device="cuda").squeeze(1)
-
-    def fwd():
-        if provider == "liger":
-            return liger_ce(_input, target)
-        else:
-            return torch_ce(_input, target)
-
-    quantiles = [0.5, 0.2, 0.8]
-
-    if mode == "forward":
-        ms, min_ms, max_ms = triton.testing.do_bench(fwd, quantiles=quantiles, rep=100)
-    elif mode == "backward":
-        y = fwd()
-
-        ms, min_ms, max_ms = triton.testing.do_bench(
-            lambda: y.backward(retain_graph=True),
-            quantiles=quantiles,
-            grad_to_none=[_input],
-            rep=100,
-        )
-    elif mode == "full":
-
-        def full():
-            y = fwd()
-            y.backward()
-
-        ms, min_ms, max_ms = triton.testing.do_bench(full, quantiles=quantiles, rep=100)
-
-    return ms
-    # return ms, min_ms, max_ms
-
-#             x_names=["V"],
-#             x_vals=[2**i for i in range(12, 18)],
-#             xlabel="vocab size",
-#             line_arg="provider",
-#             line_vals=["liger", "huggingface"],
-#             line_names=[
-#                 "Liger",
-#                 "Hugging Face",
-#             ],
-#             styles=[
-#                 ("blue", "solid"),
-#                 ("orange", "solid"),
-#             ],
-#             ylabel="time (ms)",
-#             plot_name="cross-entropy-fwd-speed-benchmark",
-#             args={"B": 8, "T": 2048, "mode": "forward", "dtype": torch.bfloat16},
-#         ),
-
-# bench_test_fn = bench_speed_cross_entropy
-# providers = ["liger", "huggingface"]
-# x_values = [2**i for i in range(12, 18)]
-# modes = ["forward", "full"]
-# extra_args = {"B": 8, "T": 2048}
-
-
-
-def run_benchmarks(
-    bench_test_fn: Callable,
-    kernel_name: str,
-    metric_name: str,
-    metric_unit: str,
-    x_name: str,
-    x_label: str,
-    x_values: List[Union[float, int]],
-    kernel_providers: List[str],
-    kernel_operation_modes: Optional[List[str]] = [None],
-    extra_benchmark_config: Optional[Dict[str, Any]] = None
-):
-    """
-    Run benchmarks given a bench_test_fn that takes in a SingleBenchmarkRunInput
-    a set of x_values to run, provider (e.g. liger, huggingface), optional mode (e.g. forward, backward, full)
-    to run the kernel, and extra_args for running the benchmark functions. Extra_args should 
-    """
-
-    assert len(kernel_operation_modes) >= 1
-    assert len(kernel_providers) >= 1
-
-    gpu_name = get_gpu_name()
-    benchmark_data_list = []
-    for kernel_operation_mode in kernel_operation_modes:
-        for kernel_provider in kernel_providers:
-            y_values = []
-            for x in x_values:
-                single_benchmark_run_input = SingleBenchmarkRunInput(
-                    x=x,
-                    kernel_provider=kernel_provider,
-                    kernel_operation_mode=kernel_operation_mode,
-                    extra_benchmark_config=extra_benchmark_config)
-                benchmark_result = bench_test_fn(single_benchmark_run_input)
-                y_values.append(benchmark_result)
-            
-            benchmark_run_data = BenchmarkData(
-                kernel_name=kernel_name,
-                kernel_operation_mode=kernel_operation_mode,
-                kernel_provider=kernel_provider,
-                metric_name=metric_name,
-                metric_unit=metric_unit,
-                gpu_name=gpu_name,
-                x_name=x_name,
-                x_label=x_label,
-                x_values=x_values,
-                y_values=y_values,
-                extra_benchmark_config=json.dumps(extra_benchmark_config),
-                timestamp=time.time()
-            )
-
-            benchmark_data_list.append(benchmark_run_data)
-
-    update_benchmark_data_csv(benchmark_data_list=benchmark_data_list)
-            
-
-
-# def benchmark_speed_cross_entropy_wrapper():
-#     curr_dir = get_current_file_directory()
-#     dir_name = "cross_entropy_speed"
-#     output_dir = os.path.join(curr_dir, dir_name)
-#     os.makedirs(output_dir, exist_ok=True)
-#     dfs = bench_speed_cross_entropy.run(
-#         save_path=output_dir, print_data=False, return_df=True
-#     )
-#     for df in dfs:
-#         print(df.head())
-#         print(df.info())
-
-
-@triton.testing.perf_report(
-    [
-        triton.testing.Benchmark(
-            x_names=["V"],
-            x_vals=[2**i for i in range(12, 18)],
-            xlabel="vocab size",
-            line_arg="provider",
-            line_vals=["liger", "huggingface"],
-            line_names=[
-                "Liger",
-                "Hugging Face",
-            ],
-            styles=[
-                ("blue", "solid"),
-                ("orange", "solid"),
-            ],
-            ylabel="GPU memory usage (MB)",
-            plot_name="cross-entropy-memory-benchmark",
-            args={"B": 8, "T": 2048, "dtype": torch.bfloat16},
-        )
-    ]
-)
-def bench_memory_cross_entropy(B, T, V, provider, dtype, device="cuda"):
-    torch_ce = CrossEntropyLoss()
-    liger_ce = LigerCrossEntropyLoss()
 
     _input = torch.randn(B * T, V, requires_grad=True, device="cuda")
     target = torch.randint(V, (B * T, 1), device="cuda").squeeze(1)
@@ -265,30 +33,78 @@ def bench_memory_cross_entropy(B, T, V, provider, dtype, device="cuda"):
         y = fwd()
         y.backward()
 
-    mem = _test_memory(full)
-    return mem / 2**20
+    mem_mean, mem_std = _test_memory(full)
+    return SingleBenchmarkRunOuput(
+        y_mean=mem_mean,
+        y_std=mem_std,
+    )
 
 
-# def benchmark_memory_cross_entropy_wrapper():
-#     curr_dir = get_current_file_directory()
-#     dir_name = "cross_entropy_memory"
-#     output_dir = os.path.join(curr_dir, dir_name)
-#     os.makedirs(output_dir, exist_ok=True)
-#     bench_memory_cross_entropy.run(save_path=output_dir, print_data=True)
+def bench_speed_cross_entropy(input: SingleBenchmarkRunInput) -> SingleBenchmarkRunOuput:
+    torch_ce = CrossEntropyLoss()
+    liger_ce = LigerCrossEntropyLoss()
+
+    V = input.x
+    provider = input.kernel_provider
+    mode = input.kernel_operation_mode
+    B = input.extra_benchmark_config["B"]
+    T = input.extra_benchmark_config["T"]
+
+    _input = torch.randn(B * T, V, requires_grad=True, device="cuda")
+    target = torch.randint(V, (B * T, 1), device="cuda").squeeze(1)
+
+    def fwd():
+        if provider == "liger":
+            return liger_ce(_input, target)
+        else:
+            return torch_ce(_input, target)
+
+    if mode == "forward":
+        y_mean = triton.testing.do_bench(fwd, rep=100, return_mode="mean")
+    elif mode == "backward":
+        y = fwd()
+
+        y_mean = triton.testing.do_bench(
+            lambda: y.backward(retain_graph=True),
+            grad_to_none=[_input],
+            rep=100,
+            return_mode="mean",
+        )
+    elif mode == "full":
+
+        def full():
+            y = fwd()
+            y.backward()
+
+        y_mean = triton.testing.do_bench(full, rep=100, return_mode="mean")
+
+    return SingleBenchmarkRunOuput(
+        y_mean=y_mean,
+    )
 
 
 if __name__ == "__main__":
+    common_configs = {
+        "kernel_name": "cross_entropy",
+        "x_name": "V",
+        "x_label": "vocab size",
+        "x_values": [2**i for i in range(12, 18)],
+        "kernel_providers": ["liger", "huggingface"],
+        "extra_benchmark_configs": [{"B": 8, "T": 2048}],
+        "overwrite": True
+    }
+
     run_benchmarks(
         bench_test_fn=bench_speed_cross_entropy,
-        kernel_name="cross_entropy",
-        kernel_operation_modes=["forward"],
+        kernel_operation_modes=["forward", "full"],
         metric_name="speed",
         metric_unit="ms",
-        x_name="V",
-        x_label="vocab size",
-        x_values=[2**i for i in range(12, 18)],
-        kernel_providers=["liger", "huggingface"],
-        extra_benchmark_config={"B": 8, "T": 2048}
+        **common_configs
     )
-    # benchmark_speed_cross_entropy_wrapper()
-    # benchmark_memory_cross_entropy_wrapper()
+    run_benchmarks(
+        bench_test_fn=bench_memory_cross_entropy,
+        kernel_operation_modes=["full"],
+        metric_name="memory",
+        metric_unit="MB",
+        **common_configs
+    )
--- benchmark/scripts/utils.py
+++ benchmark/scripts/utils.py
@@ -1,65 +1,111 @@
 import os
 import time
 from dataclasses import asdict, dataclass
-from typing import Callable, List, Any, Dict, Optional
+from typing import Callable, List, Any, Dict, Optional, Union, Tuple
 import csv
 import torch
+from importlib.metadata import version
+import numpy as np
+from itertools import zip_longest
+from collections import OrderedDict
+import json
 
+
+LIGER_KERNEL_VERSION = version('liger-kernel')
 
 @dataclass
 class SingleBenchmarkRunInput:
-    x: int
+    x: Union[int, float]
     kernel_provider: str
-    kernel_operation_mode: str
-    extra_benchmark_config: Dict[str, Any]
+    kernel_operation_mode: Optional[str] = ""
+    extra_benchmark_config: Optional[Dict[str, Any]] = None
+
+@dataclass
+class SingleBenchmarkRunOuput:
+    y_mean: float
+    # Triton 3.0.0 doesn't yet support returning all values from do_bench(), so make this optional
+    # This change adds support for return_mode='all': https://github.com/triton-lang/triton/pull/4493 
+    y_std: Optional[float] = None
 
 
 @dataclass
 class BenchmarkData:
     """
-    BenchmarkData is a dataclass to store the benchmark data for a single benchmark run.
-    For example, the data collected after running rms_norm speed benchmark from a single provider.
+    BenchmarkData is a dataclass to store the benchmark data for a a completed benchmark
+    run on all x-values for a given kernel/kernel operation mode/metric/extra_benchmark_config
     """
     kernel_name: str
-    kernel_operation_mode: str
     kernel_provider: str
     metric_name: str
     metric_unit: str
     gpu_name: str
     x_name: str
     x_label: str
-    x_values: List[Any]
-    y_values: List[Any]
-    extra_benchmark_config: str
+    x_values: List[float]
+    y_values_mean: List[float]
     timestamp: str
+    kernel_operation_mode: Optional[str] = None
+    y_values_std: Optional[List[float]] = None
+    extra_benchmark_config_str: Optional[str] = None
+    liger_version: str = LIGER_KERNEL_VERSION
 
 
 @dataclass
 class BenchmarkDataCSVRow:
+    # The ordering of field names here will be the order of columns in the CSV
     kernel_name: str
-    kernel_operation_mode: str
     kernel_provider: str
+    kernel_operation_mode: Union[str, None]
     metric_name: str
     metric_unit: str
-    gpu_name: str
     x_name: str
     x_label: str
     x_value: float
-    y_value: float
-    extra_benchmark_config: str
+    y_value_mean: float
+    y_value_std: Union[str, None]
+    extra_benchmark_config_str: Union[str, None]
+    gpu_name: str
     timestamp: str
+    liger_version: str
 
 
-def _test_memory(func: Callable, _iter: int = 10) -> float:
+def get_mean_std_stats(values: List[float]) -> Tuple[float, float]:
+    """
+    Calculate the mean and standard deviation of the values.
+
+    Args:
+        values: The list of values.
+
+    Returns:
+        Tuple[float, float]: The mean and standard deviation of the values.
+    """
+    return np.mean(values), np.std(values)
+
+
+def _test_memory(func: Callable, _iter: int = 10) -> Tuple[float, float]:
+    """
+    Run the function `func` `_iter` times and return the mean and standard deviation of the
+    peak allocated memory in MB.
+
+    Args:
+        func: The function to run.
+        _iter: The number of times to run the function.
+
+    Returns:
+        Tuple[float, float]: The mean and standard deviation of the peak allocated memory in MB.
+    """
     total_mem = []
 
     for _ in range(_iter):
         torch.cuda.memory.reset_peak_memory_stats()
         func()
+        # Memory returned in bytes
         mem = torch.cuda.max_memory_allocated()
         total_mem.append(mem)
 
-    return sum(total_mem) / len(total_mem)
+    mean, std = get_mean_std_stats(total_mem)
+    # Convert bytes to MB
+    return mean / 2**20, std / 2**20
 
 
 def get_current_file_directory() -> str:
@@ -84,17 +130,14 @@ def sleep(seconds):
     return decorator
 
 
-def _print_memory_banner():
+def _print_benchmarking_banner(metric_name: str, kernel_name: str):
     print("**************************************")
-    print("*     BENCHMARKING GPU MEMORY        *")
-    print("**************************************")
-
-
-def _print_speed_banner():
-    print("**************************************")
-    print("*        BENCHMARKING SPEED          *")
+    print(f"     BENCHMARKING {metric_name.upper()} for {kernel_name.upper()}")
     print("**************************************")
 
+
+def get_formatted_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S")
 
 def get_gpu_name():
     """
@@ -118,26 +161,125 @@ def update_benchmark_data_csv(
     If an entry already exists for the benchmark, then overwrite it if `overwrite` is True.
     """
 
+    def create_unique_key(row):
+        # This unique key is used to determine if a benchmark run already exists in the CSV
+        # If the key is the same, then the benchmark run already exists and will optionally
+        # be overwritten. Otherwise, it is considered a new benchmark run and appended.
+        return (
+            row["kernel_name"],
+            row["kernel_provider"],
+            row["kernel_operation_mode"],
+            row["metric_name"],
+            row["x_name"],
+            row["x_value"],
+            row["extra_benchmark_config_str"],
+            row["gpu_name"],
+        )
+
     fieldnames = BenchmarkDataCSVRow.__annotations__.keys()
 
     # Make filename path relative to current file
     filename_abs_path = os.path.join(get_current_file_directory(), "../data", filename)
     file_exists = os.path.isfile(filename_abs_path)
 
-    with open(filename_abs_path, mode="a") as file:
+    # Read existing data into a list of dicts
+    existing_data = []
+    if file_exists:
+        with open(filename_abs_path, mode="r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                existing_data.append(row)
+
+    existing_data_dict = OrderedDict((create_unique_key(row), row) for row in existing_data)
+
+    for benchmark_data in benchmark_data_list:
+        benchmark_data_dict = asdict(benchmark_data)
+        x_values = benchmark_data_dict.pop("x_values")
+        y_values_mean = benchmark_data_dict.pop("y_values_mean")
+        y_values_std = benchmark_data_dict.pop("y_values_std", [])
+
+        # Need to convert benchmark_data into multiple rows based on x_values and y_values
+        for x_value, y_value_mean, y_value_std in zip_longest(x_values, y_values_mean, y_values_std):
+            row = BenchmarkDataCSVRow(
+                x_value=x_value, y_value_mean=y_value_mean, y_value_std=y_value_std, **benchmark_data_dict
+            )
+            row_dict = asdict(row)
+
+            row_key = create_unique_key(row_dict)
+
+            if row_key in existing_data_dict:
+                if overwrite:
+                    existing_data_dict[row_key] = row_dict
+            else:
+                existing_data_dict[row_key] = row_dict
+
+
+    with open(filename_abs_path, mode="w", newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
 
-        if not file_exists:
-            writer.writeheader()
+        for row in existing_data_dict.values():
+            writer.writerow(row)
 
-        for benchmark_data in benchmark_data_list:
-            benchmark_data_dict = asdict(benchmark_data)
-            x_values = benchmark_data_dict.pop("x_values")
-            y_values = benchmark_data_dict.pop("y_values")
+def run_benchmarks(
+    bench_test_fn: Callable,
+    kernel_name: str,
+    metric_name: str,
+    metric_unit: str,
+    x_name: str,
+    x_label: str,
+    x_values: List[Union[float, int]],
+    kernel_providers: List[str],
+    kernel_operation_modes: Optional[List[str]] = [None],
+    extra_benchmark_configs: Optional[List[Dict[str, Any]]] = None,
+    overwrite: bool = False,
+):
+    """
+    Run benchmarks given a bench_test_fn that takes in a SingleBenchmarkRunInput
+    a set of x_values to run, provider (e.g. liger, huggingface), optional mode (e.g. forward, backward, full)
+    to run the kernel, and extra_args for running the benchmark functions. Extra_args should 
+    """
 
-            # Need to convert benchmark_data into multiple rows based on x_values and y_values
-            for x_value, y_value in zip(x_values, y_values):
-                row = BenchmarkDataCSVRow(
-                    x_value=x_value, y_value=y_value, **benchmark_data_dict
+    assert len(kernel_operation_modes) >= 1
+    assert len(kernel_providers) >= 1
+
+    _print_benchmarking_banner(metric_name=metric_name, kernel_name=kernel_name)
+
+    gpu_name = get_gpu_name()
+    benchmark_data_list = []
+    for extra_benchmark_config in extra_benchmark_configs:
+        for kernel_operation_mode in kernel_operation_modes:
+            for kernel_provider in kernel_providers:
+                y_values_mean = []
+                y_values_std = []
+
+                for x in x_values:
+                    single_benchmark_run_input = SingleBenchmarkRunInput(
+                        x=x,
+                        kernel_provider=kernel_provider,
+                        kernel_operation_mode=kernel_operation_mode,
+                        extra_benchmark_config=extra_benchmark_config)
+                    benchmark_result: SingleBenchmarkRunOuput = bench_test_fn(single_benchmark_run_input)
+                    y_values_mean.append(benchmark_result.y_mean)
+                    y_values_std.append(benchmark_result.y_std)
+                
+                benchmark_run_data = BenchmarkData(
+                    kernel_name=kernel_name,
+                    kernel_operation_mode=kernel_operation_mode,
+                    kernel_provider=kernel_provider,
+                    metric_name=metric_name,
+                    metric_unit=metric_unit,
+                    gpu_name=gpu_name,
+                    x_name=x_name,
+                    x_label=x_label,
+                    x_values=x_values,
+                    y_values_mean=y_values_mean,
+                    y_values_std=y_values_std,
+                    extra_benchmark_config_str=json.dumps(extra_benchmark_config),
+                    timestamp=get_formatted_time(),
+                    liger_version=LIGER_KERNEL_VERSION
                 )
-                writer.writerow(asdict(row))
+
+                benchmark_data_list.append(benchmark_run_data)
+
+    update_benchmark_data_csv(benchmark_data_list=benchmark_data_list, overwrite=overwrite)
